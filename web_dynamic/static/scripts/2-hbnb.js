30c89ca4bb417744f1215996ceadc5a5b5ba2633
JDorangetree <julian.naranjo2014@gmail.com>
2020-05-18 12:16:47 -0500

    [Feature] task3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     let amenities = [];
     $('input[type="checkbox"]').change(function(){
-        if($(this).is(":checked") == true){
+        if($(this).is(":checked") === true){
             amenities.push($(this).val());
         }
         else if($(this).is("checked") == false){
@@ -10,11 +10,13 @@ $(document).ready(function(){
             return item !== value
         })
         }
+
         $('.amenities h4').text((amenities).join(', '));
     });
-    $.get('http://0.0.0.0:5001/api/v1/status', function (data, textStatus){
-        if (textStatus === 200) {
+    $.get('http://0.0.0.0:5001/api/v1/status', function (data){
+        if (data.status === "OK") {
             $('#api_status').addClass('available')
+            $('#api_status').removeAttr('id');
         }
     })
 });
